749729b39e7a5ee6030a7d287f1cdbe2e8d175da
ozonecoders <46056448+ozonecoders@users.noreply.github.com>
2020-06-08 00:00:00 +0430

    os

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -1,6 +1,6 @@
+import os
 if not __name__.endswith("sample_config"):
-    import sys
-    import os
+    import sys    
     print("The README is there to be read. Extend this sample config to a config file, don't just rename and change "
           "values here. Doing that WILL backfire on you.\nBot quitting.", file=sys.stderr)
     quit(1)
